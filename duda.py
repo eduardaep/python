890553d9a5162cae58b17dfe96320e5ddb2796f6
eduardaep <86934697+eduardaep@users.noreply.github.com>
2021-07-05 12:41:55 -0300

    Add files via upload

---
+++ duda.py
@@ -0,0 +1,76 @@
+"""Áries: de 21 março a 20 abril;
+Touro: de 21 abril a 20 maio;
+Gêmeos: de 21 maio a 20 junho;
+Câncer: de 21 junho a 22 julho;
+Leão: de 23 julho a 22 agosto;
+Virgem: de 23 agosto a 22 setembro;
+Libra: de 23 setembro a 22 outubro;
+Escorpião: de 23 outubro a 21 novembro;
+Sagitário: de 22 novembro a 21 dezembro;
+Capricórnio: de 22 dezembro a 20 janeiro;
+Aquário: de 21 janeiro a 18 fevereiro;
+Peixes: de 19 fevereiro a 20 março;Áries: de 21 março a 20 abril;
+Touro: de 21 abril a 20 maio;
+Gêmeos: de 21 maio a 20 junho;
+Câncer: de 21 junho a 22 julho;
+Leão: de 23 julho a 22 agosto;
+Virgem: de 23 agosto a 22 setembro;
+Libra: de 23 setembro a 22 outubro;
+Escorpião: de 23 outubro a 21 novembro;
+Sagitário: de 22 novembro a 21 dezembro;
+Capricórnio: de 22 dezembro a 20 janeiro;
+Aquário: de 21 janeiro a 18 fevereiro;
+Peixes: de 19 fevereiro a 20 março;
+ 
+"""  
+dia = int(input("digite sua data de nascimento: ")) 
+mes = input("digite seu mes de nascimento:" )
+
+if(dia <=20 and mes == "marco: "): 
+   print("aries") 
+elif (dia >= 21 and mes =="abril: "):
+  print("aries") 
+elif (dia <=20 and mes == "abril: "):
+   print("touro")
+elif (dia >=21 and mes == "maio: "):
+   print("touro")
+elif (dia <=20 and mes == "maio: "):
+    print("gemeos")
+elif(dia >=21 and mes == "junho: "):
+    print("gemeos") 
+elif(dia <=20 and mes == "junho: "):
+    print("cancer")
+elif(dia >=21 and mes == "julho: "):
+    print("cancer")
+elif(dia <=20 and mes == "julho: "):
+    print("leao")
+elif(dia >=21 and mes == "agosto: "):
+    print("leao")
+elif(dia <=20 and mes == "agosto: "):
+    print("virgem")
+elif(dia >=21 and mes == "setembro: "):
+    print("virgem")
+elif(dia <=20 and mes == "setembro: "):
+    print("libra")
+elif(dia >=21 and mes == "outubro: "):
+    print("libra")
+elif(dia <=20 and mes == "outubro: "):
+    print("escorpiao")
+elif(dia >=21 and mes == "novembro: "):
+    print("escorpiao")
+elif(dia <=20 and mes == "novembro: "):
+    print("sagitario")
+elif(dia >=21 and mes == "dezembro: "):
+    print("sagitario")
+elif(dia <=20 and mes == "janeiro: "):
+    print("capricornio")
+elif(dia >=21 and mes == "janeiro: "):
+    print("capricornio")
+elif(dia <=20 and mes == "fevereiro: "):
+    print("aquario")
+elif(dia >=21 and mes == "fevereiro: "):
+    print("aquario")
+elif(dia <=20 and mes == "marco: "):
+    print("peixes")
+
+
